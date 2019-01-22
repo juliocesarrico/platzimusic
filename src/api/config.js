edaf7f023f9cfc205108f7cd6075ecdb91fb82b1
juliorico <julio.rico@hasltda.com>
2019-01-22 16:41:59 -0500

    inicial commit

---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+export default {
+    appName: 'platzimusic',
+    apiKey: 'fb3bc4f5d7bffadba5fa6c54f7b6500a',
+    secret:  '658c74d3152dd27c050b7b25d0a82a45',
+    registeredTo: 'juliorico6'
+}
+
+// Application name	platzimusic
+// API key	fb3bc4f5d7bffadba5fa6c54f7b6500a
+// Shared secret	658c74d3152dd27c050b7b25d0a82a45
+// Registered to	juliorico6
